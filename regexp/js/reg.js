a05c3a80a20d2b76df12c9144b2cd3cdbc9322b9
jsz315 <jsz_thing@126.com>
2016-05-19 16:30:22 +0800

    home

---
+++ regexp/js/reg.js
@@ -0,0 +1,52 @@
+$(function(){
+	$(".url-box .btn").click(function(){
+		var url = $(".url-box .input-txt").val();
+		$.post("/reg/url", {url: url}, function(data){
+			$(".data-txt").val(data);
+		});
+
+	});
+
+	$(".match-btn").click(function(){
+		var str = $(".data-txt").val();
+		var regexp = new RegExp($(".find").val(), $(".param").val());
+		var rs = str.match(regexp);
+		
+		$(".source")[0].innerHTML = regexp.source;
+		rs && $(".result-txt").val(rs.join("\n"));
+	});
+
+	$(".exec-btn").click(function(){
+		var str = $(".data-txt").val();
+		var regexp = new RegExp($(".find").val(), $(".param").val());
+		var rs = null;
+		var list = [];
+		while(rs = regexp.exec(str)){
+			for(var i = 0; i < rs.length; i++){
+				list.push(rs[i]);
+			}
+		}
+		
+		$(".source")[0].innerHTML = regexp.source;
+		$(".result-txt").val(list.join("\n"));
+	});	
+
+	$(".replace-btn").click(function(){
+		var str = $(".data-txt").val();
+		var regexp = new RegExp($(".find").val(), $(".param").val());
+		var rs = str.replace(regexp, $(".replace").val());
+		
+		$(".source")[0].innerHTML = regexp.source;
+		$(".result-txt").val(rs);
+	});
+
+	$(".split-btn").click(function(){
+		var str = $(".data-txt").val();
+		var regexp = new RegExp($(".find").val(), $(".param").val());
+		var rs = str.split(regexp);
+		
+		$(".source")[0].innerHTML = regexp.source;
+		rs && $(".result-txt").val(rs.join("\n"));
+	});
+
+});
