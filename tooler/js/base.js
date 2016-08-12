f02d3ea7ef4a3ecb4669a2d271e26da20dc248cf
jsz315 <jsz_thing@126.com>
2016-08-12 21:02:54 +0800

    整合

--- tooler/js/base.js
+++ tooler/js/base.js
@@ -2,43 +2,88 @@
  * Created by jiangsizhi on 2016/8/11.
  */
 $(function(){
-    $(".ps-txt").val(getSaveData("ps"));
-    $(".ls-txt").val(getSaveData("ls"));
 
-    $(".data-btn").click(function(){
-        var str = $(".input-txt").val();
-        var list = str.split("\n");
-        var aim = [];
-        var ps = $(".ps-txt").val();
-        var ls = $(".ls-txt").val();
-        setSaveData("ps", ps);
-        setSaveData("ls", ls);
-        for(var i = 0; i < list.length; i++){
-            aim.push(ps + list[i] + ls);
+    readData(["url", "start", "end", "reg", "find", "replace", "before", "after", "width", "height"]);
+
+    $(".web-list-create").click(function(){
+        var url = $(".url").val();
+        var start = $(".start").val();
+        var end = $(".end").val();
+        start = parseInt(start);
+        end = parseInt(end);
+        var list = [];
+        for(var i = start; i <= end; i++){
+            var aim = url.replace("###", i);
+            list.push(aim);
         }
-        $(".output-txt").val(aim.join("\n"));
+        $(".webs").val(list.join("\n"));
+        saveData(["url", "start", "end"]);
     });
 
-    $(".img-btn").click(function(){
-        var str = $(".output-txt").val();
-        var list = str.split("\n");
-        var aim = [];
-        for(var i = 0; i < list.length; i++){
-            var li = '<li class="img-item"><a target="_blank" href="' + list[i] + '"><img src="' + list[i] + '"></a></li>';
-            aim.push(li);
-        }
-        $(".img-list")[0].innerHTML = aim.join("");
+    $(".web-grab").click(function(){
+        var reg = $(".reg").val();
+        var webs = $(".webs").val();
+        var urls = webs.split("\n");
+        var param = {};
+        param.urls = JSON.stringify(urls);
+        param.reg = reg;
+        $.post("http://127.0.0.1:3000/grab", param, function(str){
+            $(".fetch").val(str);
+            $(".data").val(str);
+        });
+        saveData(["reg"]);
     });
 
-    function getSaveData(key){
-        var str = localStorage.getItem(key);
-        if(!str){
-            str = "";
+    $(".deal-with").click(function(){
+        var find = $(".find").val();
+        var replace = $(".replace").val();
+        var before = $(".before").val();
+        var after = $(".after").val();
+        var fetch = $(".fetch").val();
+        var list = fetch.split("\n");
+        var aim = [];
+        for(var i = 0; i < list.length; i++){
+            var str = list[i];
+            if(find != ""){
+                var reg = new RegExp(find, "ig");
+                str = str.replace(reg, replace);
+            }
+            str = before + str + after;
+            aim.push(str);
         }
-        return str;
-    }
+        $(".data").val(aim.join("\n"));
+        saveData(["find", "replace", "before", "after"]);
+    });
 
-    function setSaveData(key, value){
+    $(".img-show").click(function(){
+        var data = $(".data").val();
+        var width = $(".width").val();
+        var height = $(".height").val();
+        width = width == "" ? "auto" : width;
+        height = height == "" ? "auto" : height;
+        var list = data.split("\n");
+        $(".img-list")[0].innerHTML = modelData("img-item", {list: list, width: width, height: height});
+        saveData(["width", "height"]);
+    });
+});
+
+function saveData(list){
+    for(var i = 0; i < list.length; i++){
+        var key = list[i];
+        var value = $("." + key).val();
         localStorage.setItem(key, value);
     }
-});
+}
+
+function readData(list){
+    for(var i = 0; i < list.length; i++){
+        var key = list[i];
+        var value = localStorage.getItem(key);
+        $("." + key).val(value);
+    }
+}
+
+function modelData(id, data){
+    var html = new EJS({text:$("#" + id)[0].innerHTML, type:"{"}).render(data);
+    return html;
+}
