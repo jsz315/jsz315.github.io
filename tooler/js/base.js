51a3a43358daef0097682d711316300701b7e676
jsz315 <jsz_thing@126.com>
2019-07-14 04:49:39 +0800

    添加跳转

--- tooler/js/base.js
+++ tooler/js/base.js
@@ -3,6 +3,8 @@
  */
 $(function(){
 
+    addJump();
+
     readData(["url", "start", "end", "reg", "find", "replace", "before", "after", "width", "height"]);
 
     $(".web-list-create").click(function(){
@@ -97,6 +99,49 @@ function getFomratNum(total, i){
         return i;
     }
     var list = new Array(Number(num)).fill(0);
-    var str = list.push(i).join("");
+    list.push(i);
+    var str = list.join("");
     return str.substr(-num);
+}
+
+function addJump(){
+    const VID = "JUMP_PAGE";
+    const TYPE_TOP = "▲";
+    const TYPE_BOTTOM = "▼";
+
+    if(!document.querySelector(VID)){
+        addDiv(TYPE_TOP);
+        addDiv(TYPE_BOTTOM);
+    }
+
+    function addDiv(type){
+        var div = document.createElement("div");
+        div.className = VID;
+        document.body.appendChild(div);
+        div.style.display = "block";
+        div.style.color = "#ffffff";
+        div.style.textAlign = "center";
+        div.style.lineHeight = "40px";
+        div.style.width = "40px";
+        div.style.height = "40px";
+        div.style.position = "fixed";
+        div.style.bottom = type == TYPE_BOTTOM ? "20px" : "70px";
+        div.style.right = "20px";
+        div.style.zIndex = "3000";
+        div.style.background = "rgba(0, 0, 0, 0.4)";
+        div.style.borderRadius = "8px";
+        div.style.cursor = "pointer";
+        div.innerHTML = type;
+
+        div.onclick = function(){
+            if(type == TYPE_BOTTOM){
+                document.documentElement.scrollTop = document.documentElement.scrollHeight;
+            }
+            else{
+                document.documentElement.scrollTop = 0;
+            }
+            
+        }
+    }
+
 }
